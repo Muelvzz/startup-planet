5bcc9af96da9b103c7492eede7ab50718878f705
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-08-05 19:02:25 +0800

    aside: sending a response

--- server.js
+++ server.js
@@ -3,4 +3,13 @@ import express from "express"
 const PORT = 8000
 const app = express()
 
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham"
+}
+
+app.get("/", (req, res) => {
+  res.json(celebrity)
+})
+
 app.listen(PORT, () => console.log(`Connected to port ${PORT}`))
