8b0e79a277f178d2cdcc144653af0860725ae1b3
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-08-05 19:45:33 +0800

    feature: get request

--- server.js
+++ server.js
@@ -4,8 +4,40 @@ import { startups } from "./data.js"
 const PORT = 8000
 const app = express()
 
-app.get("/", (req, res) => {
-  res.json(startups)
+app.get("/api", (req, res) => {
+  const { 
+    industry, 
+    country, 
+    continent, 
+    is_seeking_funding, 
+    has_mvp 
+  } = req.query
+
+  let filteredData = startups
+
+  if (industry) {
+    filteredData = filteredData.filter((data) => data.industry.toLowerCase() === industry.toLowerCase())
+  }
+
+  if (country) {
+    filteredData = filteredData.filter((data) => data.country.toLowerCase() === country.toLowerCase())
+  }
+
+  if (continent) {
+    filteredData = filteredData.filter((data) => data.continent.toLowerCase() === continent.toLowerCase())
+  }
+
+  if (is_seeking_funding !== undefined) {
+    const isSeeking = is_seeking_funding === "true"
+    filteredData = filteredData.filter((data) => data.is_seeking_funding === isSeeking)
+  }
+
+  if (has_mvp !== undefined) {
+    const hasMvp = has_mvp === "true"
+    filteredData = filteredData.filter((data) => data.has_mvp === hasMvp)
+  }
+
+  res.json(filteredData)
 })
 
 app.listen(PORT, () => console.log(`Connected to port ${PORT}`))
