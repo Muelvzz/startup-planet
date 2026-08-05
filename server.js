017accae87199b718fe03654291f361bce291728
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-08-05 19:09:58 +0800

    feature: serving data

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 import express from "express"
+import { startups } from "./data.js"
 
 const PORT = 8000
 const app = express()
 
-const celebrity = {
-  type: "action hero",
-  name: "JSON Statham"
-}
-
 app.get("/", (req, res) => {
-  res.json(celebrity)
+  res.json(startups)
 })
 
 app.listen(PORT, () => console.log(`Connected to port ${PORT}`))
